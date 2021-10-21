ddddc67b443a343f230370d8d65398806be6c54c
Tomé Carvalho <tomecarvalho@ua.pt>
2021-10-21 15:31:46 +0100

    Attempt to use city name as argument instead of ID
    Commented out because the "cityMap" HashMap isn't working, for reasons unbeknownst to me.
    The .get returns null even though the key exists.

--- lab1/lab1_5/IpmaApiClient/src/main/java/ipmaclient/GetForecast.java
+++ lab1/lab1_5/IpmaApiClient/src/main/java/ipmaclient/GetForecast.java
@@ -1,15 +1,57 @@
 package ipmaclient;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GetForecast {
+    private final static String districtCapitals = "https://api.ipma.pt/open-data/distrits-islands.json";
+
+    public static String cityForecast(String cityName, boolean print) {
+        try {
+            URL url = new URL(districtCapitals);
+            URLConnection request = url.openConnection();
+            request.connect();
+            JsonParser jp = new JsonParser();
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+            JsonObject rootobj = root.getAsJsonObject();
+            JsonArray data = rootobj.getAsJsonArray("data");
+            Map<String, Integer> cityMap = new HashMap<>();
+            for (int i = 0; i < data.size(); i++) {
+                cityMap.put(
+                        data.get(i).getAsJsonObject().get("local").toString().strip(),
+                        Integer.parseInt(data.get(i).getAsJsonObject().get("globalIdLocal").toString().strip())
+                );
+            }
+            System.out.println("cityMap: " + cityMap);
+            System.out.println("cityName: " + "'" + cityName + "'");
+            System.out.println(cityMap.keySet());
+            // TODO: Why does cityMap.get(cityName) return null even though the key exists?
+            System.out.println("cityMap.get(cityName): " + cityMap.get(cityName));
+
+            // return cityForecast(cityMap.get(cityName), print);
+            return cityForecast(1100900, print); // Hard-coded Leiria since the map is cursed
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error retrieving forecast (exception).";
+        }
+    }
+
     public static String cityForecast(int cityId, boolean print) {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://api.ipma.pt/open-data/")
