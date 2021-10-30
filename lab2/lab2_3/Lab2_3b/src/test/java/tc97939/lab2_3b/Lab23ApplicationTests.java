6bc16495ec97cff77217f63982bae39d2adc970e
TomeCarvalho <tomecarvalho@ua.pt>
2021-10-30 14:39:41 +0100

    Add missing exercises

---
+++ lab2/lab2_3/Lab2_3b/src/test/java/tc97939/lab2_3b/Lab23ApplicationTests.java
@@ -0,0 +1,13 @@
+package tc97939.lab2_3b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
